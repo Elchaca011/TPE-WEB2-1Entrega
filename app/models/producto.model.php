f90f02b2c51ddd26bb3bb42b5908158ab0c400f2
iñaki gopar <tatigop09@gmail.com>
2023-10-08 13:24:26 -0300

    modificacion del tpe 8/10/23

--- app/models/producto.model.php
+++ app/models/producto.model.php
@@ -1,11 +1,27 @@
 <?php
+//Incluyo el config.php el cual se encarga de la conexion a la db
+require_once("config.php");
 class ProductoModel{
     //Atributo
     private $db;
 
     //Constructor
     function __construct(){
-        $this->db = new PDO("mysql:host=localhost;dbname=db_tpe;charset=utf8", "root", "");
+        $this->db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";" . DB_Charset , DB_USER , DB_PASS);
+    }
+
+    //Metodos
+
+    //Obtiene y devuelve de la base de datos todos los productos
+    public function getProductos(){
+        //Envía la consulta SQL con JOIN para obtener todos los productos con la información de la categoría a la que pertenece
+        $query = $this->db->prepare("SELECT p.*, c.categoria AS nombre_categoria
+                                    FROM productos AS p
+                                    JOIN categorias AS c ON p.id_categoria = c.id_categoria");
+        $query->execute();
+        $productos = $query->fetchAll(PDO::FETCH_OBJ);
+
+        return $productos;
     }
 
     //Obtiene y devuelve de la base de datos todos los productos de una determinada categoria.
@@ -14,10 +30,18 @@ class ProductoModel{
         $query = $this->db->prepare("SELECT * FROM productos WHERE id_categoria = ?");
         $query->execute([$id_fk]);
     
-        // $productos es un arreglo de productos
-        $productos = $query->fetchAll(PDO::FETCH_OBJ);
+        // $productosXCategoria es un arreglo de productos por una determinada categoria
+        $productosXCategoria = $query->fetchAll(PDO::FETCH_OBJ);
     
-        return $productos;
+        return $productosXCategoria;
+    }
+
+    public function agregarProducto($id_categoria,$nombre,$material,$precio){
+        //Blindeo(Protego) los parametreos con VALUES(?,?,?,?) (seguridad)
+        $query = $this->db->prepare("INSERT INTO productos (id_categoria,nombre,material,precio)VALUES(?,?,?,?)");
+        $query->execute([$id_categoria,$nombre,$material,$precio]);
+
+        return $this->db->lastInsertId();
     }
     
 
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+//El config se encarga de tener los datos de conexion a la db
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "db_tpe");
+define("DB_Charset", "utf8mb4");
